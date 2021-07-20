e259d360366f2318a0364186d01e8dc0198ffce7
yona3 <kou.y.0905@icloud.com>
2021-07-21 08:44:36 +0900

    ✨ Add AuthForm component

---
+++ src/components/AuthForm.tsx
@@ -0,0 +1,78 @@
+import type { ChangeEvent, FormEvent, VFC } from "react";
+import { useState } from "react";
+import { useForm } from "src/hooks/form";
+import type { Keys } from "src/types";
+
+import { Input } from "./Input";
+
+export const AuthForm: VFC = () => {
+  const [formType, setFormType] = useState<"register" | "login">("register");
+  const { values, handleSetValue } = useForm({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChangeFormType = (e: FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setFormType((prev) => (prev === "register" ? "login" : "register"));
+  };
+
+  const handleChangeFormValue = (e: ChangeEvent<HTMLInputElement>) =>
+    handleSetValue(e.target.name as Keys, e.target.value);
+
+  return (
+    <form className="py-5 px-5 mx-auto max-w-xl bg-gray-600 rounded-md">
+      <h1 className="text-2xl font-semibold text-white">
+        {formType === "register" ? "Register" : "Login"}
+      </h1>
+
+      <div className="flex flex-col mt-4 space-y-3">
+        {formType === "register" && (
+          <Input
+            name="name"
+            type="text"
+            value={values.name}
+            onChange={handleChangeFormValue}
+            placeholder="name"
+          />
+        )}
+        <Input
+          name="email"
+          type="email"
+          value={values.email}
+          onChange={handleChangeFormValue}
+          placeholder="email"
+        />
+        <Input
+          name="password"
+          type="password"
+          value={values.password}
+          onChange={handleChangeFormValue}
+          placeholder="password"
+        />
+      </div>
+
+      <div className="flex justify-between items-center pr-4 mt-4">
+        <button
+          className="
+        border border-gray-600 focus:outline-none
+        py-2 px-6 text-white bg-gray-700 rounded 
+      "
+          type="submit"
+        >
+          submit
+        </button>
+
+        {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
+        <button
+          className="text-white underline cursor-pointer"
+          onClick={handleChangeFormType}
+          onSubmit={handleChangeFormType}
+        >
+          {formType === "register" ? "Login" : "Register"}
+        </button>
+      </div>
+    </form>
+  );
+};
