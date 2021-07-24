cf16befdb851b01fbee449f11a8f99662e179899
yona3 <kou.y.0905@icloud.com>
2021-07-24 22:11:22 +0900

    🚨 Fix type definitions

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,9 +2,9 @@ import type { ReactNode, VFC } from "react";
 
 import { Header } from "./Header";
 
-interface Props {
+type Props = {
   children: ReactNode;
-}
+};
 
 export const Layout: VFC<Props> = ({ children }) => {
   return (
